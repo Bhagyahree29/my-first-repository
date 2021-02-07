f9ceccb4b32500803daffbb700a14fa299f3ebbe
Bhagyashree Durgekar <78657518+Bhagyahree29@users.noreply.github.com>
2021-02-07 20:39:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator_using_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
